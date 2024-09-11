a77284972ddfbd0d97f5eb6353271969626a2bdf
Rwan Adel <rwan32310@gmail.com>
2024-09-11 03:07:44 +0300

    userId

--- controller/posts.js
+++ controller/posts.js
@@ -1,17 +1,15 @@
-const Post =require("../models/post");
+const Post = require("../models/post");
 const AppError = require("../utils/AppError");
 const logger = require("../utils/logger");
 
-
-
 exports.getAllPosts = async (req, res, next) => {
-    try {
-        const posts = await Post.find();
-        res.send(posts);
-    } catch (err) {
-        logger.error(`Error getting posts: ${err.message}`);
-        next(err);
-    }
+  try {
+    const posts = await Post.find();
+    res.send(posts);
+  } catch (err) {
+    logger.error(`Error getting posts: ${err.message}`);
+    next(err);
+  }
 };
 //////////////////////////////////////////////////////////////////
 
@@ -26,50 +24,50 @@ exports.getAllPosts = async (req, res, next) => {
 // };
 
 exports.getuserPosts = async (req, res, next) => {
-    try {
-      const postId = req.params.id;
-      const post = await Post.findById(postId);
-      if (!post) {
-        return next(new AppError("No post found with that ID", 404));
-      }
-      res.send({ status: "success", data: { post } });
-    } catch (err) {
-      logger.error(`Error getting post: ${err.message}`);
-      next(err);
+  try {
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+    if (!post) {
+      return next(new AppError("No post found with that ID", 404));
     }
-  };
+    res.send({ status: "success", data: { post } });
+  } catch (err) {
+    logger.error(`Error getting post: ${err.message}`);
+    next(err);
+  }
+};
 ////////////////////////////////////////////////////////////////////
 exports.createPost = async (req, res, next) => {
-    try {
-        const { title, description, images } = req.body;
-        const post = new Post({ title, description, images });
-        await post.save();
-        res.send({ msg: "Post created", post });
-    } catch (err) {
-        logger.error(`Error creating post: ${err.message}`);
-        next(err);
-    }
+  try {
+    const { title, description, images, userId } = req.user._id;
+    const post = new Post({ title, description, images });
+    await post.save();
+    res.send({ msg: "Post created", post });
+  } catch (err) {
+    logger.error(`Error creating post: ${err.message}`);
+    next(err);
+  }
 };
 
 exports.upDatepost = async (req, res, next) => {
-    try {
-        const newTitle = req.body.title;
-        const oldTitle = req.params.title;
-        await Post.updateOne({ title: oldTitle }, req.body);
-        res.send("Update done");
-    } catch (err) {
-        logger.error(`Error updating post: ${err.message}`);
-        next(err);
-    }
+  try {
+    const newTitle = req.body.title;
+    const oldTitle = req.params.title;
+    await Post.updateOne({ title: oldTitle }, req.body);
+    res.send("Update done");
+  } catch (err) {
+    logger.error(`Error updating post: ${err.message}`);
+    next(err);
+  }
 };
 
 exports.deletePost = async (req, res, next) => {
-    try {
-        const deletedpost = req.params.title;
-        await Post.deleteOne({ title: deletedpost });
-        res.send("Deleted done");
-    } catch (err) {
-        logger.error(`Error deleting post: ${err.message}`);
-        next(err);
-    }
+  try {
+    const deletedpost = req.params.title;
+    await Post.deleteOne({ title: deletedpost });
+    res.send("Deleted done");
+  } catch (err) {
+    logger.error(`Error deleting post: ${err.message}`);
+    next(err);
+  }
 };
